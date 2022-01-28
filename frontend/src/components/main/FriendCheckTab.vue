53bf04fa9f0ff593f4cf9e1b83010dc80bddf84b
1gj2wo3tjr <1gj2wo3tjr@naver.com>
2022-01-28 16:08:19 +0900

    STYLE: 이미지 클릭 시 친구 tab 활성화 --> button 클릭 시 친구 tab 활성화,  각 component style scoped 처리

--- frontend/src/components/main/FriendCheckTab.vue
+++ frontend/src/components/main/FriendCheckTab.vue
@@ -6,7 +6,7 @@
         <div>
           {{ friend.name }}
         </div>
-        <div class="d-flex mt-2">
+        <div class="d-flex mt-2 button-box">
           <button class="">수락</button>
           <button class="ms-3">거절</button>
         </div>
@@ -37,6 +37,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.button-box {
+  font-size: 12px;
+}
 
+.button-box > button {
+  background: #FDFAF3;
+  font-size: 12px;
+  border-radius: 15px;
+  width: 90px;
+}
 </style>
