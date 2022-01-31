58df2660f03671f1460097ec82f75b0607a298d8
1gj2wo3tjr <1gj2wo3tjr@naver.com>
2022-01-31 13:18:49 +0900

    STYLE: main page 옷장 layout 설계 axios 제외

--- frontend/src/components/main/HostCloset.vue
+++ frontend/src/components/main/HostCloset.vue
@@ -1,13 +1,55 @@
 <template>
-  <div></div>
+  <div>
+    <div class="container">
+      <div class="host-name">
+        김명섭 님의 옷장
+      </div>
+      <!-- 옷장에서 옷 가져오기 -->
+      <host-closet-item class="host-closet-item"></host-closet-item>
+    </div>
+  </div>
 </template>
 
 <script>
+import HostClosetItem from './HostClosetItem.vue'
 export default {
-    name: 'HostCloset'
+  name: 'HostCloset',
+  components: { HostClosetItem },
 }
 </script>
 
-<style>
+<style scoped>
+.container {
+  width: 457px;
+  height: 535px;
+  padding: 0px;
+  position: relative;
+  top: 30px;
+  background-color: white;
+}
 
+.host-name {
+  font-size: 20px;
+}
+
+.host-closet-item {
+  margin-top: 44px;
+  height: 463px;
+  /* background-color: beige; */
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+.host-closet-item::-webkit-scrollbar {
+  width: 10px;
+  /* background-color: beige; */
+}
+.host-closet-item::-webkit-scrollbar-thumb {
+  background-color: #2f3542;
+  border-radius: 10px;
+}
+.host-closet-item::-webkit-scrollbar-track {
+  background-color: #FDFAF3;
+  border-radius: 10px;
+  box-shadow: inset 0px 0px 5px white;
+}
 </style>
---
+++ frontend/src/components/main/HostClosetItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="row">
+      <div v-for="friend in friends" :key="friend.id" class="col-4">
+        <img :src="friend.src" alt="cloth-img">
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'HostClosetItem',
+  setup() {
+    const friends= ref([
+      {id: 1, src: require('@/assets/shirt.jpg'), name:'김'},
+      {id: 2, src: require('@/assets/shirt.jpg'), name:'배'},
+      {id: 3, src: require('@/assets/shirt.jpg'), name:'이'},
+      {id: 4, src: require('@/assets/shirt.jpg'), name:'최'},
+      {id: 5, src: require('@/assets/shirt.jpg'), name:'박'},
+      {id: 6, src: require('@/assets/shirt.jpg'), name:'안'},
+      {id: 7, src: require('@/assets/shirt.jpg'), name:'강'},
+      {id: 8, src: require('@/assets/shirt.jpg'), name:'허'},
+      {id: 9, src: require('@/assets/shirt.jpg'), name:'양'},
+      {id: 10, src: require('@/assets/shirt.jpg'), name:'장'},
+    ])
+
+    return {
+      friends,
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 125px;
+  height: 150px;
+  padding: 0px;
+}
+
+</style>
