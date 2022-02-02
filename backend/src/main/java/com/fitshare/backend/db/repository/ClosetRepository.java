1d3267a084e35baa85eea3cab4c85fe539f117b7
송예진 <rkrud232@naver.com>
2022-02-02 14:03:32 +0900

    Remove: Closet, GroupChat repository 삭제

--- backend/src/main/java/com/fitshare/backend/db/repository/ClosetRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fitshare.backend.db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClosetRepository extends JpaRepository<Closet, Long> {
-}
