cd9b0ccf5603b8471dcce7d70142b36189fda259
김혜지 <hengzizng@naver.com>
2022-02-05 04:05:10 +0900

    FIX: FriendRequestRepository 의 Entity Friend -> FriendRequest 로 수정

--- backend/src/main/java/com/fitshare/backend/db/repository/FriendRequestRepository.java
+++ backend/src/main/java/com/fitshare/backend/db/repository/FriendRequestRepository.java
@@ -1,9 +1,9 @@
 package com.fitshare.backend.db.repository;
 
-import com.fitshare.backend.db.entity.Friend;
+import com.fitshare.backend.db.entity.FriendRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FriendRequestRepository extends JpaRepository<Friend, Long> {
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
 }
