992d3bc02a6e20539939724029275f03483f192f
송예진 <rkrud232@naver.com>
2022-02-04 18:35:43 +0900

    STYLE: 오타 수정, 쇼핑룸 조회 JPQL 수정

--- backend/src/main/java/com/fitshare/backend/common/model/ResponseMessage.java
+++ backend/src/main/java/com/fitshare/backend/common/model/ResponseMessage.java
@@ -4,8 +4,8 @@ public class ResponseMessage {
     private ResponseMessage() {}
 
     // Shopping Room
-    public static final String MAKE_SHOPPING_ROOM = "쇼핑룽 생성 성공입니다.";
-    public static final String ENTER_SHOPPING_ROOM = "쇼핑룽 입장 성공입니다.";
+    public static final String MAKE_SHOPPING_ROOM = "쇼핑룸 생성 성공입니다.";
+    public static final String ENTER_SHOPPING_ROOM = "쇼핑룸 입장 성공입니다.";
     public static final String EXIT_SHOPPING_ROOM = "쇼핑룸 나가기 성공입니다.";
     public static final String GET_SHOPPING_LIST = "쇼핑룸 목록 조회 성공입니다.";
 
