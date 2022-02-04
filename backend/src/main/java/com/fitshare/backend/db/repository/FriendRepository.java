2c6b9609faa05ab23af0159b6ef97613cdc8bc20
김혜지 <hengzizng@naver.com>
2022-02-05 04:21:19 +0900

    STYLE: FriendRepository 주석 추가

--- backend/src/main/java/com/fitshare/backend/db/repository/FriendRepository.java
+++ backend/src/main/java/com/fitshare/backend/db/repository/FriendRepository.java
@@ -12,6 +12,10 @@ import java.util.List;
 @Repository
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
+    /**
+     * friend table 에서 memberId 와 friendId 로 두 사용자가 이미 친구인지 판단하기 위해 사용한다
+     * return 값이 0이라면 친구가 아니고, 1이라면 친구라고 판단한다
+     */
     int countByMemberIdAndFriendId(Long memberId, Long friendId);
 
     /**
@@ -39,6 +43,9 @@ public interface FriendRepository extends JpaRepository<Friend, Long> {
             " ORDER BY m.name")
     List<FriendRes> findByMemberIdAndFriendEmailLike(@Param("memberId") Long memberId, @Param("friendEmail") String friendEmail);
 
+    /**
+     * friend table 에서 memberId 의 친구인 friendId 를 삭제한다
+     */
     int removeByMemberIdAndFriendId(Long memberId, Long friendId);
 
 }
