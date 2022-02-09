5111e93128a23764ea06f6f8331a45676056de12
이명주 <myungju25@naver.com>
2022-02-10 00:47:59 +0900

    FEAT: Redis 설정, Service 추가 [#S06P12A405-168]

---
+++ backend/src/main/java/com/fitshare/backend/api/service/RedisService.java
@@ -0,0 +1,12 @@
+package com.fitshare.backend.api.service;
+
+public interface RedisService {
+
+    void setData(String token, String id);
+
+    String getData(String token);
+
+    void delData(String token);
+
+    void setDataExpire(String token,String id,long duration);
+}
