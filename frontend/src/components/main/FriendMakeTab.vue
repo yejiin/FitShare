4df5df662027aab6f1f78fb77f451ca7ef193ecb
1gj2wo3tjr <1gj2wo3tjr@naver.com>
2022-02-10 02:06:07 +0900

    STYLE: Email로 User 검색 시 이미 친구인 사람들은 보이지 않게 코드 수정

--- frontend/src/components/main/FriendMakeTab.vue
+++ frontend/src/components/main/FriendMakeTab.vue
@@ -10,7 +10,8 @@
     >
     
     <!-- 이름, 이메일로 검색 시 나오는 users들 components -->
-    <searched-users :friends="state.friends"></searched-users>
+    <!-- <searched-users :friends="state.friends"></searched-users> -->
+    <searched-users :friends="state.unique"></searched-users>
 
 
   </div>
@@ -29,7 +30,9 @@ export default {
   },
   setup() {
     const state = reactive({
-      friends: []
+      searchedUsers: [],
+      exitsfriends: [],
+      unique: []
     })
 
     const { cookies } = useCookies() 
@@ -46,20 +49,53 @@ export default {
     const SearchUser = ref('');
 
     // input에 email 입력 시 server에 요청
-    const SearchUserEmail = () => {
+    // const SearchUserEmail = () => {
+    //   if (SearchUser.value) {
+    //     axios({
+    //       method: 'GET',
+    //       url: `http://i6a405.p.ssafy.io:8081/api/v1/members/${SearchUser.value}`,
+    //       headers: setToken()
+    //       })
+    //       .then(res => {
+    //         console.log(res)
+    //         state.friends = res.data.data
+    //       })
+    //   }
+    //   else{
+    //     state.friends = []
+    //   }
+    // }
+
+    // test
+    const SearchUserEmail = async () => {
+      const friendList = await axios({
+        method: 'GET',
+        url: 'http://i6a405.p.ssafy.io:8081/api/v1/friends',
+        headers: setToken()
+      })
+        .then(res => {
+          console.log(res)
+          state.exitsfriends = res.data.data
+        })
+        console.log(friendList)
+
       if (SearchUser.value) {
-        axios({
+        const Users = await axios({
           method: 'GET',
           url: `http://i6a405.p.ssafy.io:8081/api/v1/members/${SearchUser.value}`,
           headers: setToken()
           })
           .then(res => {
             console.log(res)
-            state.friends = res.data.data
+            state.searchedUsers = res.data.data
+            const ids = state.exitsfriends.map(e => e.id) // 친구 리스트 배열에서 각 객체의 id 값
+            state.unique = state.searchedUsers.filter(e => !ids.includes(e.id)) // email 검색으로 나온 user 배열의 각 객체의 id와 비교하여 없는 경우에만 저장
+            console.log(JSON.stringify(state.unique))
           })
+          console.log(Users)
       }
       else{
-        state.friends = []
+        state.unique = []
       }
     }
 
