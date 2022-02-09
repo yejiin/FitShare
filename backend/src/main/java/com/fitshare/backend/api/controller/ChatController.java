fce32729a5928bd25fc684d5ab8e274ca1ed3df5
김혜지 <hengzizng@naver.com>
2022-02-09 17:52:49 +0900

    FIX: 테스트코드 오류 제거

--- backend/src/main/java/com/fitshare/backend/api/controller/ChatController.java
+++ backend/src/main/java/com/fitshare/backend/api/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.fitshare.backend.api.controller;
 
 import com.fitshare.backend.api.request.ChatReq;
-import com.fitshare.backend.common.auth.JwtUtil;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -16,10 +15,6 @@ public class ChatController {
     @ApiOperation(value = "채팅", notes = "메시지 전송")
     @MessageMapping(value = "/chat/message")
     public void sendMessage(ChatReq chatReq) {
-        System.out.println("senderId = " + chatReq.getSenderId());
-        Long memberId = JwtUtil.getCurrentId().orElse(null);
-        System.out.println("memberId = " + memberId);
-
         template.convertAndSend("/topic/room/" + chatReq.getRoomId(), chatReq);
     }
 }
