ce8426170be66a8789a2879bd96174461519a991
김혜지 <hengzizng@naver.com>
2022-02-09 17:53:22 +0900

    STYLE: ChatReq 전송자 아이디 -> 이름 으로 수정

--- backend/src/main/java/com/fitshare/backend/api/request/ChatReq.java
+++ backend/src/main/java/com/fitshare/backend/api/request/ChatReq.java
@@ -14,8 +14,8 @@ public class ChatReq {
     @ApiModelProperty(name = "쇼핑룸 Id", example = "1")
     private int roomId;
 
-    @ApiModelProperty(name = "메시지 전송자 Id", example = "1")
-    private Long senderId;
+    @ApiModelProperty(name = "메시지 전송자 이름", example = "홍길동")
+    private String senderName;
 
     @ApiModelProperty(name = "전송한 메시지 내용", example = "Hello")
     private String message;
