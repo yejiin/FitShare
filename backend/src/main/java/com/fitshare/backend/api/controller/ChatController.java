609632d871362391818d983cb5694877e4bd0a30
김혜지 <hengzizng@naver.com>
2022-02-09 15:54:11 +0900

    FEAT: 단체채팅 인증과정 추가

--- backend/src/main/java/com/fitshare/backend/api/controller/ChatController.java
+++ backend/src/main/java/com/fitshare/backend/api/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.fitshare.backend.api.controller;
 
-import com.fitshare.backend.api.request.ChatDTO;
+import com.fitshare.backend.api.request.ChatReq;
+import com.fitshare.backend.common.auth.JwtUtil;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -14,7 +15,11 @@ public class ChatController {
 
     @ApiOperation(value = "채팅", notes = "메시지 전송")
     @MessageMapping(value = "/chat/message")
-    public void sendMessage(ChatDTO chatDTO) {
-        template.convertAndSend("/topic/room/" + chatDTO.getRoomId(), chatDTO);
+    public void sendMessage(ChatReq chatReq) {
+        System.out.println("senderId = " + chatReq.getSenderId());
+        Long memberId = JwtUtil.getCurrentId().orElse(null);
+        System.out.println("memberId = " + memberId);
+
+        template.convertAndSend("/topic/room/" + chatReq.getRoomId(), chatReq);
     }
 }
--- backend/src/main/java/com/fitshare/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/fitshare/backend/config/WebSocketConfig.java
@@ -1,12 +1,30 @@
 package com.fitshare.backend.config;
 
+import com.fitshare.backend.common.auth.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JwtTokenProvider tokenProvider;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 클라이언트가 WebSocket Handshake로 커넥션을 생성할 HTTP URL endpoint
@@ -25,6 +43,58 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // SEND frame일 때 사용된다
         registry.setApplicationDestinationPrefixes("/app");
     }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+                System.out.println("command : " + accessor.getCommand());
+
+                if (StompCommand.CONNECT.equals(accessor.getCommand()) ||
+                        StompCommand.SUBSCRIBE.equals(accessor.getCommand()) ||
+                        StompCommand.SEND.equals(accessor.getCommand()) ||
+                        StompCommand.MESSAGE.equals(accessor.getCommand())
+                ) {
+                    String token = accessor.getFirstNativeHeader("Authorization").substring(7);
+                    if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
+                        Authentication authentication = tokenProvider.getAuthentication(token);
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+//                        message = getNewMessage(message);
+                    }
+                }
+
+                return message;
+            }
+        });
+    }
+
+//    private Message<?> getNewMessage(Message<?> message) {
+//        Message newMessage = null;
+//        String payloadString = new String((byte[]) message.getPayload());
+//
+//        if (payloadString.isEmpty()) {
+//            return message;
+//        }
+//
+//        Long id = JwtUtil.getCurrentId().orElse(null);
+//
+//        JSONParser jsonParser = new JSONParser();
+//        JSONObject payloadObject = null;
+//        try {
+//            payloadObject = (JSONObject) jsonParser.parse(payloadString);
+//            payloadString = payloadObject.toJSONString();
+//            newMessage = MessageBuilder.createMessage(payloadString.getBytes(), message.getHeaders());
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        String newPayloadString = new String((byte[]) newMessage.getPayload());
+//
+//        return newMessage;
+//    }
 
     // TODO: buffer size 설정
     // TODO: 클라이언트 연결이 끊겼을 때 재연결 코드 작성
